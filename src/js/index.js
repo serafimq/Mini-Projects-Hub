74c41178e3fc51efa61d34444abe833b626f2c76
Yar <serafim90@mail.ru>
2023-07-31 16:31:30 +0300

    ended setting up webpack, removed old features

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,3 @@
-console.log('hello world');
+import '../index.html';
+
+import '../css/style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,170 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const isDev = process.env.MODE === 'development';
 const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all',
+        },
+        minimize: true,
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+            new ImageMinimizerPlugin({
+                minimizer: {
+                  implementation: ImageMinimizerPlugin.imageminMinify,
+                  options: {
+                    plugins: [
+                      ["gifsicle", { interlaced: true }],
+                      ["jpegtran", { progressive: true }],
+                      ["optipng", { optimizationLevel: 5 }],
+                      [
+                        "svgo",
+                        {
+                          plugins: [
+                            {
+                              name: "preset-default",
+                              params: {
+                                overrides: {
+                                  removeViewBox: false,
+                                  addAttributesToSVGElement: {
+                                    params: {
+                                      attributes: [
+                                        { xmlns: "http://www.w3.org/2000/svg" },
+                                      ],
+                                    },
+                                  },
+                                },
+                              },
+                            },
+                          ],
+                        },
+                      ],
+                    ],
+                  },
+                },
+            }),
+        ]
+    }
+    return config;
+  }
+
+const buildPlugins = () => {
+    const basePlugins = [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/[name].[contenthash:8].css',
+            chunkFilename: './css/[name].[contenthash:8].css',
+        }),
+        new CopyPlugin({
+            patterns: [
+              { 
+                from: path.resolve(__dirname, 'src', 'assets'), 
+                to: path.join(__dirname, 'dist') },
+            ],
+        })
+    ]
+
+    if (isProd) {}
+    return basePlugins;
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: isDev ? 'development' : 'production',
     entry: './js/index.js',
     output: {
-        filename:   '[name].[contenthash].js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[hash][ext][query]',
+        publicPath: '',
         clean: true
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-            minify: {
-                collapseWhitespace: isProd
+    devServer: {
+        historyApiFallback: true,
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        open: true,
+        hot: true,
+        compress: true,
+        port: 9000,
+    },
+    optimization: optimization(),
+    plugins: buildPlugins(),
+    devtool: isDev ? 'source-map' : undefined,
+    module: {
+      rules: [
+        {
+            test: /\.html$/i,
+            loader: "html-loader"
+        },
+        {
+            test:  /\.css$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+               "css-loader",
+              ]
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: (resourcePath, context) => {
+                      return path.relative(path.dirname(resourcePath), context) + '/';
+                    },
+                }
+            },
+             "css-loader",
+             "sass-loader"
+            ]
+        },
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ],
+              plugins: ['@babel/plugin-proposal-class-properties']
             }
-        })
-    ],
+          }
+        },
+        {
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            type: 'asset/resource'
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            type: "asset",
+          },
+        {
+            test: /\.(woff2|woff)$/i,
+            type: 'asset/resource',
+            generator: {
+              filename: 'fonts/[hash][ext][query]'
+            }
+        },
+        
+      ],
+    },
 }
