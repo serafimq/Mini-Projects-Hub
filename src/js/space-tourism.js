1d4bf0352fb461f141578bfdf88be560b59dd5aa
Yar <serafim90@mail.ru>
2023-08-04 17:06:36 +0300

    created mian page

--- src/js/space-tourism.js
+++ src/js/space-tourism.js
@@ -8,14 +8,17 @@ import '../css/space-tourism/_main.scss';
 
 const navigation = document.querySelector('.navigation__list');
 const navToggle = document.querySelector('.navigation__toggle');
+const body = document.querySelector('.page__body');
 
 navToggle.addEventListener('click', () => {
     const visibility = navigation.getAttribute('data-visible');
     if (visibility === 'false') {
         navigation.setAttribute('data-visible', true);
         navToggle.setAttribute('aria-expanded', true);
+        body.classList.add('menu-open');
     } else {
         navigation.setAttribute('data-visible', false);
         navToggle.setAttribute('aria-expanded', false);
+        body.classList.remove('menu-open');
     }
 })
